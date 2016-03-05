6d52abd2070f7f676a78a4e856f0ebdfe7c14f9f
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-03-05 18:20:00 +0200

    dependency injection in work

--- src/main/java/rover/RoverApplication.java
+++ src/main/java/rover/RoverApplication.java
@@ -1,6 +1,11 @@
 package rover;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.name.Named;
+import com.hubspot.dropwizard.guice.GuiceBundle;
 import com.meltmedia.dropwizard.mongo.MongoBundle;
+import com.mongodb.DB;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -13,6 +18,7 @@ import rover.resources.MainResource;
 public class RoverApplication extends Application<RoverConfiguration>{
 
     private MongoBundle<RoverConfiguration> mongoBundle;
+    private GuiceBundle<RoverConfiguration> guiceBundle;
 
     public static void main(String[] args) throws Exception {
         new RoverApplication().run(args);
@@ -28,18 +34,36 @@ public class RoverApplication extends Application<RoverConfiguration>{
         bootstrap.addBundle(mongoBundle = MongoBundle.<RoverConfiguration>builder()
                 .withConfiguration(RoverConfiguration::getMongo)
                         .build());
+
+        bootstrap.addBundle(guiceBundle = GuiceBundle.<RoverConfiguration>newBuilder()
+        .addModule(new AbstractModule() {
+            @Override protected void configure() {
+                bind(DB.class).toProvider(() -> mongoBundle.getDB());
+
+            }
+
+            @Provides
+            @Named("defaultName")
+            public String getDefaultName(RoverConfiguration config){
+                return config.getDefaultName();
+            }
+
+            @Provides
+            @Named("template")
+            public String getTemplate(RoverConfiguration config){
+                return config.getTemplate();
+            }
+        })
+        .setConfigClass(RoverConfiguration.class)
+        .build());
     }
 
     @Override
     public void run(RoverConfiguration configuration, Environment environment) {
-        final MainResource resource = new MainResource(
-                configuration.getTemplate(),
-                configuration.getDefaultName()
-        );
         final RoverHealthCheck healthCheck =
                 new RoverHealthCheck(configuration.getTemplate());
         environment.healthChecks().register("template", healthCheck);
-        environment.jersey().register(resource);
-//        environment.jersey().register(new RootResource(mongoBundle.getDB()));
+
+        environment.jersey().register(MainResource.class);
     }
 }
--- src/main/java/rover/resources/MainResource.java
+++ src/main/java/rover/resources/MainResource.java
@@ -2,8 +2,11 @@ package rover.resources;
 
 import com.google.common.base.Optional;
 import com.codahale.metrics.annotation.Timed;
+import com.google.inject.name.Named;
 import rover.api.Saying;
+import rover.services.Service;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -17,17 +20,20 @@ public class MainResource {
     private final String template;
     private final String defaultName;
     private final AtomicLong counter;
+    private final Service service;
 
-    public MainResource(String template, String defaultName) {
+    @Inject
+    public MainResource(@Named("template") String template, @Named("defaultName") String defaultName, Service service) {
         this.template = template;
         this.defaultName = defaultName;
         this.counter = new AtomicLong();
+        this.service = service;
     }
 
     @GET
     @Timed
     public Saying sayHello(@QueryParam("name") Optional<String> name) {
         final String value = String.format(template, name.or(defaultName));
-        return new Saying(counter.incrementAndGet(), value);
+        return new Saying(counter.incrementAndGet(), service.sayHello());
     }
 }
