4d6c57e4d0736e89c60c800818a8c0b70a3eabb4
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-03-05 18:36:35 +0200

    main domain classes

---
+++ src/main/java/rover/domain/Customer.java
@@ -0,0 +1,34 @@
+package rover.domain;
+
+/**
+ * Created by eq on 04/03/16.
+ */
+public class Customer {
+    private Long id;
+    private String name;
+    private String personalId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPersonalId() {
+        return personalId;
+    }
+
+    public void setPersonalId(String personalId) {
+        this.personalId = personalId;
+    }
+}
---
+++ src/main/java/rover/domain/Loan.java
@@ -0,0 +1,72 @@
+package rover.domain;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by eq on 04/03/16.
+ */
+public class Loan {
+    private Long id;
+    private Boolean approved;
+    private String countryRequestor;
+    private BigDecimal amount;
+    private String currency;
+    private Long term;
+    private Long customerId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getApproved() {
+        return approved;
+    }
+
+    public void setApproved(Boolean approved) {
+        this.approved = approved;
+    }
+
+    public String getCountryRequestor() {
+        return countryRequestor;
+    }
+
+    public void setCountryRequestor(String countryRequestor) {
+        this.countryRequestor = countryRequestor;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Long getTerm() {
+        return term;
+    }
+
+    public void setTerm(Long term) {
+        this.term = term;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+}
