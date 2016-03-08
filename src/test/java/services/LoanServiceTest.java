b86799da6840975e68a62c445da85f4febb9b0cb
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-03-08 03:49:24 +0200

    tests for loan service

--- src/test/java/services/LoanServiceTest.java
+++ src/test/java/services/LoanServiceTest.java
@@ -1,5 +1,7 @@
 package services;
 
+import com.mongodb.BasicDBObject;
+import org.bson.types.ObjectId;
 import org.jongo.Jongo;
 import org.jukito.JukitoRunner;
 import org.jukito.UseModules;
@@ -7,7 +9,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import rover.dao.CustomerDao;
+import rover.domain.Customer;
+import rover.domain.Loan;
+import rover.services.LoanService;
+import rover.utils.JongoFilter;
 import testHelpers.TestModule;
+
+import java.math.BigDecimal;
 
 /**
  * Created by eq on 08/03/16.
@@ -24,10 +33,33 @@ public class LoanServiceTest extends Assert {
         jongo.getCollection(LOANS).drop();
         jongo.getCollection(CUSTOMERS).drop();
         jongo.getCollection(BLACKLIST).drop();
+        jongo.getDatabase().createCollection(LOANS, new BasicDBObject());
+        jongo.getDatabase().createCollection(CUSTOMERS,new BasicDBObject());
+        jongo.getDatabase().createCollection(BLACKLIST, null);
     }
 
     @Test
-    public void injectionTest(){
-        assertEquals("foobar", "foobar");
+    public void processNewLoan(LoanService loanService, CustomerDao customerDao){
+        Loan insertedLoan = loanService.processNewLoan("111-11", "Spock", "Vulcan", BigDecimal.TEN, 5l, "LV");
+        Loan foundLoan = loanService.find(new ObjectId(insertedLoan.getId()));
+        Customer foundCustomer = customerDao.find(new ObjectId(foundLoan.getCustomerId()));
+
+        assertEquals(insertedLoan.getId(), foundLoan.getId());
+        assertEquals(insertedLoan.getCustomerId(), foundLoan.getCustomerId());
+        assertEquals(insertedLoan.getStatus(), foundLoan.getStatus());
+        assertEquals("Vulcan Spock", foundCustomer.getName());
+        assertEquals("111-11", foundCustomer.getPersonalId());
     }
+
+    @Test
+    public void twoLoansForCustomer(LoanService loanService, CustomerDao customerDao) {
+        loanService.processNewLoan("111-11", "Spock", "Vulcan", BigDecimal.TEN, 5l, "LV");
+        Loan lastLoan = loanService.processNewLoan("111-11", "Data", "Mr", BigDecimal.ONE, 5l, "LV");
+        Customer foundCustomer = customerDao.find(new ObjectId(lastLoan.getCustomerId()));
+
+        assertEquals(customerDao.load(JongoFilter.get()).size(), 1);
+        assertEquals(loanService.load(JongoFilter.get()).size(), 2);
+        assertEquals("Mr Data", foundCustomer.getName());
+    }
+
 }
