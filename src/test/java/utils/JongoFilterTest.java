837392ed91b880e89959fcf2c0dc658c8b3a44e5
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-03-08 02:56:04 +0200

    jongo syntax tests

--- src/test/java/utils/JongoFilterTest.java
+++ src/test/java/utils/JongoFilterTest.java
@@ -1,6 +1,5 @@
 package utils;
 
-import com.google.api.client.util.Sets;
 import org.junit.Assert;
 import org.junit.Test;
 import rover.utils.JongoFilter;
@@ -15,10 +14,17 @@ import java.util.Set;
 public class JongoFilterTest extends Assert {
 
     @Test
-    public void OneSimpleParam(){
-        JongoFilter f = JongoFilter.get();
-        f.addParam("aaa", 123);
-        f.buildQuery();
+    public void noParams(){
+        JongoFilter f = JongoFilter.get().buildQuery();
+        assertEquals("{}", f.getQuery());
+        assertArrayEquals(new Object[]{}, f.getArgs());
+    }
+
+    @Test
+    public void oneSimpleParam(){
+        JongoFilter f = JongoFilter.get()
+                .addParam("aaa", 123)
+                .buildQuery();
 
         Set<Object> a = new HashSet<>();
         a.add(123);
@@ -26,6 +32,43 @@ public class JongoFilterTest extends Assert {
 
         assertEquals("{ aaa: {$in: #}}", f.getQuery());
         assertArrayEquals(args, f.getArgs());
+    }
+
+    @Test
+    public void manySimpleParams(){
+        JongoFilter f = JongoFilter.get()
+                .addParam("aaa", 1)
+                .addParam("bbb", 2)
+                .buildQuery();
+
+        Set<Object> firstParam = new HashSet<>();
+        firstParam.add(1);
+        Set<Object> secondParam = new HashSet<>();
+        secondParam.add(2);
+        Object[] args = Arrays.asList(firstParam, secondParam).toArray();
+
+        assertEquals("{ aaa: {$in: #}, bbb: {$in: #}}", f.getQuery());
+        assertArrayEquals(args, f.getArgs());
+    }
+
+    @Test
+    public void complexParams(){
+        JongoFilter f = JongoFilter.get()
+                .addParam("aaa", 1)
+                .addParam("bbb", Arrays.asList("nana"))
+                .buildQuery();
+
+        Set<Object> firstParam = new HashSet<>();
+        firstParam.add(1);
+
+        Set<Object> secondParam = new HashSet<>();
+        secondParam.add(Arrays.asList("nana"));
+
+        Object[] args = Arrays.asList(firstParam, secondParam).toArray();
+
+        assertEquals("{ aaa: {$in: #}, bbb: {$in: #}}", f.getQuery());
+        assertArrayEquals(args, f.getArgs());
+
     }
 
 
