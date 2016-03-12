09e8bb24f235bc0a5750c2bb7b0dc51c59e516a1
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-03-12 18:06:36 +0200

    pseudo webservice

--- src/main/java/rover/RoverApplication.java
+++ src/main/java/rover/RoverApplication.java
@@ -5,12 +5,22 @@ import com.hubspot.dropwizard.guice.GuiceBundle;
 import com.meltmedia.dropwizard.mongo.MongoBundle;
 import com.mongodb.DB;
 import io.dropwizard.Application;
+import io.dropwizard.jersey.gzip.GZipDecoder;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import rover.health.RoverHealthCheck;
 import rover.resources.LoanResource;
 import rover.resources.MainResource;
 import rover.utils.CountryFrequencyFilter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * Created by eq on 05/03/16.
@@ -29,9 +39,18 @@ public class RoverApplication extends Application<RoverConfiguration>{
         return "hello-world";
     }
 
+    public void updateDB() throws IOException {
+        URL website = new URL("http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz");
+        GZIPInputStream gz = new GZIPInputStream(website.openStream());
+        ReadableByteChannel rb = Channels.newChannel(gz);
+        FileOutputStream f = new FileOutputStream("GeoLite2-Country.mmdb");
+        f.getChannel().transferFrom(rb, 0, Long.MAX_VALUE);
+    }
+
     @Override
     public void initialize(Bootstrap<RoverConfiguration> bootstrap) {
         System.setProperty("java.net.preferIPv4Stack" , "true");
+
         bootstrap.addBundle(mongoBundle = MongoBundle.<RoverConfiguration>builder()
                 .withConfiguration(RoverConfiguration::getMongo)
                         .build());
@@ -44,6 +63,12 @@ public class RoverApplication extends Application<RoverConfiguration>{
             }
         }).setConfigClass(RoverConfiguration.class)
         .build());
+
+        try {
+            updateDB();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
